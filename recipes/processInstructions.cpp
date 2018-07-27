d33f9ef4602286006a91eb20e036cd123b40c8d7
Nianxu Wang <nianxuwa@usc.edu>
2018-07-27 23:15:44 +0800

    Sorted recipe page

---
+++ recipes/processInstructions.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+		std::cerr << "Please provide an input file name" << std::endl;
+		return 0;
+    }
+    std::cout << "Will add <li> and </li>" << std::endl;
+
+    std::ifstream ifile(argv[1]);
+	if (ifile.fail()) {
+    	std::cerr << "Could not open file" << std::endl;
+    	return 0;
+    }
+
+    std::ofstream ofile("output.txt");
+
+    std::string line = ""; //for each line to add <li> to
+    while(getline(ifile, line)) {
+        line = "<li>" + line + "</li>";
+        ofile << line;
+    }
+
+    return 0;
+}
